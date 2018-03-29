3af7e518b9fd28b5ce236db6241260e8e0a9f7ba
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-03-29 16:37:54 -0600

    Exito

--- MonolithV4/src/java/WebService/UsuarioActividades.java
+++ MonolithV4/src/java/WebService/UsuarioActividades.java
@@ -18,11 +18,13 @@ import Objetos.Usuario;
 public class UsuarioActividades {
 
     @WebMethod(operationName = "Ingresar")
-    public void IngresarUsuario (@WebParam(name = "NombreUsuario") String NombreUsuario,
+    public boolean IngresarUsuario (@WebParam(name = "NombreUsuario") String NombreUsuario,
             @WebParam(name = "EdadUsuario") int EdadUsuario,@WebParam(name = "Pais") String Pais,
             @WebParam(name = "DireccionUsuario") String DireccionUsuario, 
             @WebParam(name = "CorreoUsuario") String CorreoUsuario,
             @WebParam(name = "PasswordUsaurio") String PasswordUsuario)  {
+        
+        boolean Exito=false;
        try{
            Database2 db=new Database2();
            Usuario user=new Usuario();   
@@ -32,11 +34,11 @@ public class UsuarioActividades {
            user.setPais(Pais);
            user.setCorreo(CorreoUsuario);
            user.setPassword(PasswordUsuario);
-           
-           
            db.IngresoUsuario(user);
+           Exito=true;
        }catch(Exception ex){
            
        }
+       return Exito;
     }
 }
