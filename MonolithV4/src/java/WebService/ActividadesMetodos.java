2d121efee5e9de8872f267e606578c8fb26e91b2
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-03-29 16:54:08 -0600

    Webservice Actividades

--- MonolithV4/src/java/WebService/ActividadesMetodos.java
+++ MonolithV4/src/java/WebService/ActividadesMetodos.java
@@ -9,20 +9,29 @@ import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import BaseDatos.Database2;
+import Objetos.Actividad;
 /**
  *
  * @author Ricardo
  */
-@WebService(serviceName = "Actividad")
-public class Actividad {
+@WebService(serviceName = "ActividadesMetodos")
+public class ActividadesMetodos {
 
     /**
      * This is a sample web service operation
      */
     @WebMethod(operationName = "IngresarActividad")
-    public void hello(@WebParam(name = "NombreActividad") String Nombre) {
+    public void hello(@WebParam(name = "NombreActividad") String NombreActividad, 
+           @WebParam(name = "UsuarioNombre") String NombreUsuario,
+           @WebParam(name = "NombreCategoria") String NombreCategoria
+           ) {
         try{
             Database2 db=new Database2();
+            Actividad act=new Actividad();
+            act.setTitulo(NombreActividad);
+            act.setUsuario(NombreUsuario);
+            act.setCategoria(NombreCategoria);
+            db.IngresoActividad(act);
         }catch(Exception ex){
             
         }
