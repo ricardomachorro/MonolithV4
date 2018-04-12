389e0b162f533695c1c0f43c7fd9cd9c06f26171
CubicNev <juan.nevtor@hotmail.com>
2018-04-11 21:10:40 -0500

    Base de datos para grupos

--- MonolithV2.sql
+++ MonolithV2.sql
@@ -1,87 +1,122 @@
+drop database if exists MonolithV2;
 create database MonolithV2;
-/*drop database MonolithV2;*/
 use MonolithV2;
 
-create table Localizacion(IDLocalizacion int auto_increment primary key not null,
-Nombre varchar (100),
-Latitud varchar(40) not null, 
-Longitud varchar (40) not null,
-Region varchar (100) not null);
+create table Localizacion(
+	IDLocalizacion int auto_increment primary key not null,
+	Nombre varchar (100),
+	Latitud varchar(40) not null, 
+	Longitud varchar (40) not null,
+	Region varchar (100) not null
+);
 
-create table Usuario(IDUsuario int auto_increment primary key not null,
-NombreUsuario varchar(100),
-Correo  varchar(70),
-Edad int,
-Pais varchar(50),
-Direccion varchar(200),
-Contrasena varchar(70),
-TipoUsuario int,
-Puntos int not null);
+create table Usuario(
+	IDUsuario int auto_increment primary key not null,
+	NombreUsuario varchar(100),
+	Correo  varchar(70),
+	Edad int,
+	Pais varchar(50),
+	Direccion varchar(200),
+	Contrasena varchar(70),
+	TipoUsuario int,
+	Puntos int not null
+);
 
 update Usuario set TipoUsuario=2 where IDUsuario=5;
 select * from Usuario;
 
 create table Validacion(
-IdAdendum int primary key not null auto_increment,
-Nombre varchar(20) not null,
-adendum varchar (50) not null
+	IdAdendum int primary key not null auto_increment,
+	Nombre varchar(20) not null,
+	adendum varchar (50) not null
 );
 
-select *from Usuario;
-
-
-create table Categoria(IDCategoria int auto_increment not null primary key,
-NombreCategoria varchar(100) not null,
-IDUsuario int not null,
-foreign key (IDUsuario) references Usuario(IDUsuario) on update cascade on delete cascade);
+create table Categoria(
+	IDCategoria int auto_increment not null primary key,
+	NombreCategoria varchar(100) not null,
+	IDUsuario int not null,
+	foreign key (IDUsuario) references Usuario(IDUsuario) on update cascade on delete cascade
+);
 select * from Categoria;
 
-create table Actividad(IDActividad int not null primary key auto_increment,
-Nombre varchar(40),
-Fecha date,
-IDCategoria int,
-/*IDLocalizacion int,*/
-Estado boolean,
-foreign key (IDCategoria) references Categoria(IDCategoria)  on update cascade on delete cascade  /*,
-foreign key (IDLocalizacion) references Localizacion(IDLocalizacion)*/);
+create table Actividad(
+	IDActividad int not null primary key auto_increment,
+	Nombre varchar(40),
+	Fecha date,
+	IDCategoria int,
+	/*IDLocalizacion int,*/
+	Estado boolean,
+	foreign key (IDCategoria) references Categoria(IDCategoria) on update cascade on delete cascade  /*,
+	foreign key (IDLocalizacion) references Localizacion(IDLocalizacion)*/
+);
 
 select * from actividad;
 delete from actividad where IDActividad>1;
 
-create table Grupo(IDGrupo int not null primary key auto_increment,
-Nombre varchar(100) not null,
-UsuarioLider int,
-PuntoReunion int,
-foreign key(UsuarioLider) references Usuario(IDUsuario),
-foreign key (PuntoReunion) references Localizacion(IDLocalizacion));
-
-create table Tarea(IDTarea int not null primary key auto_increment,
-Nombre varchar(100),
-Fecha date,
-Estado boolean);
-
-create table TareaUsuario(IDTareaUsuario int not null auto_increment primary key,
-IDTarea int,
-IDGrupo int,
-foreign key (IDTarea) references Tarea(IDTarea)
+/*Aqui empiezan las tablas para el modulo grupos :v*/
+create table Grupo(
+	IDGrupo int not null primary key auto_increment,
+	NombreGrupo nvarchar(100) not null
+	#UsuarioLider int,
+	/*PuntoReunion int,*/
+	#foreign key(UsuarioLider) references Usuario(IDUsuario) on update cascade on delete cascade/*,
+	#foreign key (PuntoReunion) references Localizacion(IDLocalizacion) on update cascade on delete cascade*/
 );
 
-create table Nota(IDNota int not null auto_increment primary key,
-Nombre varchar(100),
-Contenido text,
-IDUsuario int,
-foreign key (IDUsuario) references Usuario(IDUsuario));
+create table Tarea(
+	IDTarea int not null primary key auto_increment,
+    IDGrupo int,
+	Nombre nvarchar(100),
+	Fecha datetime,
+	Estado boolean,
+    foreign key(IDGrupo) references Grupo(IDGrupo) on update cascade on delete cascade
+);
 
+create table catRol(
+	IDRol int not null auto_increment primary key,
+    rol varchar(15)
+);
+
+create table Miembros(
+	IDMiembro int not null auto_increment primary key,
+    IDUsuario int,
+    IDGrupo int,
+    IDRol int,
+    foreign key(IDUsuario) references Usuario(IDUsuario) on update cascade on delete cascade,
+    foreign key (IDGrupo) references Grupo(IDGrupo) on update cascade on delete cascade,
+    foreign key (IDRol) references catRol(IDRol)
+);
+
+create table TareaMiembro(
+	IDTareaMiembro int not null auto_increment primary key,
+    IDTarea int,
+    IDMiembro int,
+    foreign key (IDTarea) references Tarea(IDTarea) on update cascade on delete cascade,
+    foreign key (IDMiembro) references Miembros(IDMiembro) on update cascade on delete cascade
+);
+
+insert into catRol(rol) values('Lider');
+insert into catRol(rol) values('Miembro');
+/*Aqui terminan las tablas para el modulo grupos :v */
+
+create table Nota(
+	IDNota int not null auto_increment primary key,
+	Nombre varchar(100),
+	Contenido text,
+	IDUsuario int,
+	foreign key (IDUsuario) references Usuario(IDUsuario)
+);
 select * from Nota;
 
 create table Logro(
-IDLogro int not null auto_increment primary key,
-IDUsuario int not null,
-Img int not null,
-Filtro int not null,
-Nombre varchar(100) not null,
-fecha date not null
+	IDLogro int not null auto_increment primary key,
+	IDUsuario int not null,
+	Img int not null,
+	Filtro int not null,
+	Nombre varchar(100) not null,
+	fecha date not null
 );
+
 insert into Usuario(NombreUsuario,Correo,Edad,Pais,Direccion,Contrasena,Puntos) values('memo','memo@fdsad.com',21,'mexico','dasfdsfsdfsadfsd','memo',80);
 insert into Usuario(NombreUsuario,Correo,Edad,Pais,Direccion,Contrasena,Puntos) values('memo1','memo@fdsad.com',21,'mexico','dasfdsfsdfsadfsd','memo',30);
 insert into Logro(IDUsuario,Img,Filtro,Nombre,fecha) values(1,2,5,'dogo #97','2017-05-12');
@@ -90,19 +125,19 @@ select * from Logro;
 select * from Usuario;
 
 create table Intercambio(
-IDInter int not null auto_increment primary key,
-IDusuarioDa int not null,
-IDusuarioRe int not null,
-UsuarioDa varchar(100) not null,
-UsuarioRe varchar(100) not null,
-IDdogoDa int not null,
-IDdogoRe int not null,
-FiltroDa varchar(100) not null,
-ImgdogoDa int not null,
-dogoDa varchar(100) not null,
-dogoRe int not null,
-Estado varchar(100) not null,
-fecha date not null
+	IDInter int not null auto_increment primary key,
+	IDusuarioDa int not null,
+	IDusuarioRe int not null,
+	UsuarioDa varchar(100) not null,
+	UsuarioRe varchar(100) not null,
+	IDdogoDa int not null,
+	IDdogoRe int not null,
+	FiltroDa varchar(100) not null,
+	ImgdogoDa int not null,
+	dogoDa varchar(100) not null,
+	dogoRe int not null,
+	Estado varchar(100) not null,
+	fecha date not null
 );
 select * from Intercambio;
 
@@ -110,14 +145,18 @@ create table Estampa(IDEstampa int not null auto_increment primary key,
 Nombre varchar (100),
 Costo int);
 
-create table LogroUsuario(IDLogroUsuario int not null auto_increment primary key,
-IDUsuario int not null,
-IDLogro int not null,
-foreign key (IDUsuario) references Usuario(IDUsuario),
-foreign key (IDLogro) references Logro(IDLogro));
+create table LogroUsuario(
+	IDLogroUsuario int not null auto_increment primary key,
+	IDUsuario int not null,
+	IDLogro int not null,
+	foreign key (IDUsuario) references Usuario(IDUsuario),
+	foreign key (IDLogro) references Logro(IDLogro)
+);
 
-create table  EstampaUsaurio(IDEstampaUsuario int not null auto_increment primary key,
-IDUsuario int not null,
-IDEstampa int not null,
-foreign key (IDUsuario) references Usuario(IDUsuario),
-foreign key (IDEstampa) references Estampa(IDEstampa));
+create table  EstampaUsaurio(
+	IDEstampaUsuario int not null auto_increment primary key,
+	IDUsuario int not null,
+	IDEstampa int not null,
+	foreign key (IDUsuario) references Usuario(IDUsuario),
+	foreign key (IDEstampa) references Estampa(IDEstampa)
+);
